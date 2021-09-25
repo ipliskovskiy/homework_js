70beb25e38ee6c159e2684d4a4e1c71ec54ee851
Илья Плисковский <i.pliskovskiy@city-mobil.ru>
2021-09-25 20:37:22 +0300

    Fix

--- HomeWork6/js/gallery.js
+++ HomeWork6/js/gallery.js
@@ -1,25 +1,44 @@
 var small = ["img/small/s1.png", "img/small/s2.png", "img/small/s3.png", "img/small/s4.png"];
-var big = ["img/big/b1.jpeg", "img/big/b1.jpeg", "img/big/b1.jpeg"];
 
 function initialize() {
     for(var image of small){
         var img = document.createElement("img");
         img.src = image;
+        img.id = image.split('/')[2].split('.')[0];
         img.onclick = showBigImage;
         showImage('small', img);
     }
 }
 
 
-function showBigImage(e) {
+function showBigImage() {
     var bigImage = document.createElement("img");
-    bigImage.src = "img/big/b1.jpeg";
+    bigImage.src = "img/big/b" + this.id[1] + ".jpeg";
+    bigImage.style.height='400px';
+    bigImage.style.width='500px';
     showImage('big', bigImage);
 }
 
 function showImage(place, image){
     var div = document.getElementById(place);
-    div.append(image);
+    if (place == 'big'){
+        while (div.firstChild){
+            div.removeChild(div.firstChild);
+        }
+    }
+    try {
+        // image.onerror = function(){alert('картинка не существует')};
+        image.onerror = function(){
+            div.innerHTML = "<p>Картинка не существует!</p>"
+        }
+        image.onload = function(){
+            div.append(image);
+        };
+    } catch (e){
+        console.log("Картинка не существует")
+    }
+    
+    
 }
 
 initialize()
